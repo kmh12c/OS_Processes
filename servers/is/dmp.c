fa80d96b9797588d3280bd5a16d62546e393b604
kevinshurtzacu <kms14d@acu.edu>
2017-02-21 19:03:35 +0000

    Added missing comma

--- servers/is/dmp.c
+++ servers/is/dmp.c
@@ -19,7 +19,7 @@ struct hook_entry {
 	{ F1, 	proctab_dmp, "Kernel process table" },
 	{ F2,	privileges_dmp, "Process privileges" },
 	{ F3,	image_dmp, "System image" },
-    { F4,   proctabipc_dmp, "IPC messages dump"}
+    { F4,   proctabipc_dmp, "IPC messages dump"},
 	{ F5,	monparams_dmp, "Boot monitor parameters" },
 	{ F6,	irqtab_dmp, "IRQ hooks and policies" },
 	{ F7,	kmessages_dmp, "Kernel messages" },
